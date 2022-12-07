70ac907ced346abf54b0aed4b7a2280cd5a0af98
VladisHud <savanchukvlad899@gmail.com>
2022-12-07 17:12:02 +0200

    ignore

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int repamount = 2000;
+        int amount = 100;
+        int bonus;
+        if (repamount > 1000) {
+            bonus = (repamount / 100);
+            int finamount = (amount + repamount + bonus);
+            System.out.println("Balance="+finamount);
+            System.out.println("Bonus="+bonus);
+        }
+
+    }
+}
