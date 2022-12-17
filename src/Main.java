ac9c5e4d1caf62db97839fe44174baeae05e3dcc
VladisHud <savanchukvlad899@gmail.com>
2022-12-17 14:01:45 +0200

    remaster

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        int repamount = 2000;
+        int repamount = 500;
         int amount = 100;
         int bonus;
-        if (repamount > 1000) {
+        int finamount;
+        if (repamount >= 1000) {
             bonus = (repamount / 100);
-            int finamount = (amount + repamount + bonus);
+            finamount = (amount + repamount + bonus);
+            System.out.println("Balance=" + finamount);
+            System.out.println("Bonus=" + bonus);
+        } else {
+            finamount = (amount + repamount);
+            bonus = 0;
             System.out.println("Balance=" + finamount);
             System.out.println("Bonus=" + bonus);
         }
